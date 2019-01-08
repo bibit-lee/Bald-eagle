399740bd9052919438b6262ce208f59458bef629
bibit-lee2019 <lb200610@hotmail.com>
2019-01-08 20:00:32 +0800

    2019-1-8-20:00

--- src/BackGroundPanel.java
+++ src/BackGroundPanel.java
@@ -9,17 +9,14 @@ public class BackGroundPanel {
         int scr_height=(int) dimension.getHeight();
         JPanel beijing=new JPanel();
         beijing.setOpaque(false);
-        beijing.setLayout(null );
-        beijing.setPreferredSize(new Dimension(scr_width,scr_height));
+        beijing.setLayout(null);
+        beijing.setSize(scr_width,scr_height);
         ImageIcon bjPic=new ImageIcon("C:\\Users\\dell\\IdeaProjects\\Bald-eagle\\res\\pic.jpg");
         bjPic.setImage(bjPic.getImage().getScaledInstance(scr_width,scr_height,Image.SCALE_FAST));
         JLabel bgLabel=new JLabel(bjPic);
         bgLabel.setBounds(0,0,scr_width,scr_height);
         beijing.add(bgLabel);
         beijing.setVisible(true);
-
-
-
         return beijing;
     }
 }
--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,20 +1,19 @@
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
-    JPanel beijing;
 
     public MainFrame(){
+        JPanel beijing;
         JFrame mainFrame=new JFrame();
+        mainFrame.setExtendedState(MAXIMIZED_BOTH);
         JLayeredPane jLayeredPane=new JLayeredPane();
 
-        mainFrame.setExtendedState(MAXIMIZED_BOTH);
+
         BackGroundPanel backGroundPanel=new BackGroundPanel();
         beijing=backGroundPanel.backGroundPanel();
         jLayeredPane.add(beijing,Integer.valueOf(100));
-
         mainFrame.setContentPane(jLayeredPane);
         mainFrame.setVisible(true);
-
 
     }
 
