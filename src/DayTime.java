e27ebde6d13921413967adf81cbae5a8cbcd4507
bibit-lee <lb020119>
2019-01-10 04:09:25 +0800

    1-9:22:00

---
+++ src/DayTime.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by lb200 on 2019-01-06.
+ */
+
+public class DayTime {
+    public JFrame dayTime(){
+        JTextFieldPassword jx;
+        JFrame jFrame=new JFrame();
+        jFrame.setSize(2000,1500);
+        Container c=jFrame.getContentPane();
+        JTextFieldPassword jb=new JTextFieldPassword();
+
+
+
+        return jFrame;
+    }
+    public static void main(String[] args) {
+
+    }
+}
